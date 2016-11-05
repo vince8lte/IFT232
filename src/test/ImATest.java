cc9619eba8fcb3ef1de8969c3c77b132841baff7
vince8lte <v.quernel@gmail.com>
2016-11-05 16:18:50 -0400

    repost

---
+++ src/test/ImATest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class ImATest {
+	
+	@Before
+	public void setUp()
+	{
+		
+	}
+	
+	@After
+	public void tearDown()
+	{
+		
+	}
+
+}
