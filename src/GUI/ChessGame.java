f7f0b2b65bff9c8841fa306c36c4d4b9bb65aa31
Samuel <Samuel@DESKTOP-TPCTGKG>
2016-11-18 16:54:25 -0500

    Version 0

--- src/GUI/ChessGame.java
+++ src/GUI/ChessGame.java
@@ -1,8 +1,16 @@
 package GUI;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,67 +19,94 @@ import javax.swing.JPanel;
 
 import classes.Piece;
 
-public class ChessGame extends JFrame {
+public class ChessGame extends JPanel implements ComponentListener {
+    private JFrame window;
     
-    private static String backgroundURL = "ressources/pictures/chessboard.jpg";
+    private Image background;
+    private Image scaledBackground;
+    private Image piece;
+    private Image scaledPiece;
+    
+    private double squareSizeX;
+    private double squareSizeY;
+    private double borderSizeX;
+    private double borderSizeY;
     
     private Piece[][] pieces;
-    private ImageIcon oldBackground;
-    private JLabel background;
-    private ImageIcon background2;
-    
-    private ChessGame()
-    {
-    }
     
     public ChessGame(int x, int y)
     {
-        // Initialisation de l'interface
-        background = new JLabel(new ImageIcon(new ImageIcon(backgroundURL).getImage().getScaledInstance(x, y, Image.SCALE_DEFAULT)));
-        background2 = new ImageIcon(new ImageIcon(backgroundURL).getImage().getScaledInstance(x, y, Image.SCALE_DEFAULT));
-        oldBackground = new ImageIcon(backgroundURL);
-        this.setSize(x, y);        
-        this.add(background);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+        // Initialisation de la fenêtre
+        window = new JFrame();
+        window.setSize(x, y);
+        window.setTitle("Jeu d'échec");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setResizable(true);
+        window.getContentPane().add(this, BorderLayout.CENTER);
+        window.setVisible(true);
+        window.addComponentListener(this);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Logique pour sélectionner un pion et le déplacer à faire
+                // On a accès aux variables privées de ChessGame ici
+            }
+        });
+        
+        // Initialisation du JPanel
+        this.setPreferredSize(new Dimension(x, y));
         
         // Initialisation des pièces
         pieces = new Piece[8][8];
         
         for (int X = 0; X < pieces.length; ++X)
         {
-            for (int Y = 0; Y < pieces[X].length; ++Y)
-            {
-                pieces[X][Y] = new Piece("");
-            }            
+            pieces[X][0] = new Piece("");
         }
-        
-        afficher();
     }
     
-    public void afficher()
-    {
-      /*
-        for (int X = 0; X < pieces.length; ++X)
-        {
-            for (int Y = 0; Y < pieces[X].length; ++Y)
-            {
-                ImageIcon imgPiece = new ImageIcon(pieces[X][Y].toString());
-                int f = imgPiece.getIconHeight();
-                int t = imgPiece.getIconWidth();
-                int b = oldBackground.getIconHeight();
-                
-                this.add(new JLabel(new ImageIcon(imgPiece.getImage().getScaledInstance((imgPiece.getIconWidth() * background2.getIconWidth()) / oldBackground.getIconWidth(), 
-                                           (imgPiece.getIconHeight() * background2.getIconHeight()) / oldBackground.getIconHeight(),
-                                           Image.SCALE_DEFAULT))));                       
-            }            
-        }
-        */
-        
-    }
-    
-	public static void main(String[] args) {	    
-	    ChessGame tata = new ChessGame(500, 500);
-	    tata.setVisible(true);
-	}
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(scaledBackground, 0, 0, this);
 
+        for (int x = 0; x < pieces.length; ++x)
+        {
+            for (int y = 0; y < pieces[x].length; ++y)
+            {
+                if (pieces[x][y] != null)
+                    g.drawImage(scaledPiece, (int)(x*squareSizeX+borderSizeX), (int)(y*squareSizeY+borderSizeY), this);                     
+            }
+        }
+    }
+
+    @Override
+    public void componentResized(ComponentEvent e)
+    {
+        borderSizeX = this.getWidth()*0.0625;
+        borderSizeY = this.getHeight()*0.0625;
+        squareSizeX = (this.getWidth()-borderSizeX*2.0)/8.0;
+        squareSizeY = (this.getHeight()-borderSizeY*2.0)/8.0;
+        
+        if (background == null) {
+            background = new ImageIcon("ressources/pictures/chessboard.jpg")
+                    .getImage();
+        }
+        scaledBackground = background.getScaledInstance(this.getWidth(),
+                this.getHeight(), Image.SCALE_FAST);
+        
+        if (scaledPiece == null) {
+            piece = new ImageIcon("ressources/pictures/pionn.png").getImage();
+        }
+        scaledPiece = piece.getScaledInstance((int)(piece.getWidth(null)*((squareSizeX)/piece.getWidth(null))),
+                (int)(piece.getHeight(null)*((squareSizeY)/piece.getHeight(null))), Image.SCALE_FAST);
+    }
+
+    @Override
+    public void componentShown(ComponentEvent arg0){}
+    @Override
+    public void componentHidden(ComponentEvent e){}
+    @Override
+    public void componentMoved(ComponentEvent e){}
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import GUI.ChessGame;
+
+public class Main
+{
+    public static void main(String[] args) {        
+        ChessGame chessGame = new ChessGame(500, 500);
+    }
+}
